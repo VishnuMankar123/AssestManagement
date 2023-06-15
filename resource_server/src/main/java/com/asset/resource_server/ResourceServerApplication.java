7b94b4497092114812c34d0a984d6d965fa1b5af
Devang Hemant Bhagwat <devanghb@lentra.ai>
2023-06-15 17:38:06 +0530

    Excluded UserDetailsService Auto Configuration from the Application

--- resource_server/src/main/java/com/asset/resource_server/ResourceServerApplication.java
+++ resource_server/src/main/java/com/asset/resource_server/ResourceServerApplication.java
@@ -2,8 +2,11 @@ package com.asset.resource_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = UserDetailsServiceAutoConfiguration.class
+)
 public class ResourceServerApplication {
 
     public static void main(String[] args) {
