228d1486ec9de7c0a9b055a2efd40f80e2f9238c
Devang Hemant Bhagwat <devanghb@lentra.ai>
2023-06-15 16:11:18 +0530

    Removed In-memory user details service bean from application security configuration

--- authorization_server/src/main/java/com/asset/authorization_server/config/ApplicationSecurityConfig.java
+++ authorization_server/src/main/java/com/asset/authorization_server/config/ApplicationSecurityConfig.java
@@ -33,19 +33,4 @@ public class ApplicationSecurityConfig {
     public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder(encoderStrength);
     }
-
-    /**
-     * This is just for testing purposes, I will later save all the user details to the database.
-     * @param encoder the password encoder bean
-     * @return In-memory user details manager.
-     */
-    @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
-        var user = User.withUsername("admin")
-                .password(encoder.encode("Admin@123"))
-                .authorities("admin")
-                .build();
-
-        return new InMemoryUserDetailsManager(user);
-    }
 }
