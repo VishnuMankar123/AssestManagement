26384924b4d5c9e9b8e92798d27acec613f8a9df
Devang Hemant Bhagwat <devanghb@lentra.ai>
2023-06-15 15:13:30 +0530

    Added ClientService which implements the RegisteredClientRepository

---
+++ authorization_server/src/main/java/com/asset/authorization_server/service/ClientService.java
@@ -0,0 +1,39 @@
+package com.asset.authorization_server.service;
+
+import com.asset.authorization_server.exception.ResourceNotFoundException;
+import com.asset.authorization_server.mapper.client.ClientToRegisteredClient;
+import com.asset.authorization_server.mapper.client.RegisteredClientToClient;
+import com.asset.authorization_server.repository.ClientRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ClientService implements RegisteredClientRepository {
+
+    private final ClientRepository clientRepository;
+    private final RegisteredClientToClient registeredClientToClient;
+    private final ClientToRegisteredClient clientToRegisteredClient;
+
+
+    @Override
+    public void save(RegisteredClient registeredClient) {
+        this.clientRepository.save(this.registeredClientToClient.apply(registeredClient));
+    }
+
+    @Override
+    public RegisteredClient findById(String id) {
+        return this.clientRepository.findById(Integer.parseInt(id))
+                .map(clientToRegisteredClient)
+                .orElseThrow(() -> new ResourceNotFoundException("Client", "id", id));
+    }
+
+    @Override
+    public RegisteredClient findByClientId(String clientId) {
+        return this.clientRepository.findByClientId(clientId)
+                .map(clientToRegisteredClient)
+                .orElseThrow(() -> new ResourceNotFoundException("Client", "clientId", clientId));
+    }
+}
