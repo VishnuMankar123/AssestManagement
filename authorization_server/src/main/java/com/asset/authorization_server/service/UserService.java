9c5b00e2cfff46cd0b958a527e1cd2c3a12e1be5
Devang Hemant Bhagwat <devanghb@lentra.ai>
2023-06-15 16:10:28 +0530

    Added a UserService that implements the UserDetailsService interface

---
+++ authorization_server/src/main/java/com/asset/authorization_server/service/UserService.java
@@ -0,0 +1,23 @@
+package com.asset.authorization_server.service;
+
+import com.asset.authorization_server.repository.UserRepository;
+import com.asset.authorization_server.wrapper.UserWrapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return this.userRepository.findByUsername(username)
+                .map(UserWrapper::new)
+                .orElseThrow(() -> new UsernameNotFoundException("User with username " + username + " not found"));
+    }
+}
