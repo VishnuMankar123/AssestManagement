6c40796904374087cb60aba141a9b8102ca6af1c
Devang Hemant Bhagwat <devanghb@lentra.ai>
2023-06-22 11:42:36 +0530

    Added a file for storing the commonly used constants

---
+++ resource_server/src/main/java/com/asset/resource_server/constant/CommonConstants.java
@@ -0,0 +1,8 @@
+package com.asset.resource_server.constant;
+
+public class CommonConstants {
+    private CommonConstants() {}
+
+    public static final String ID = "id";
+    public static final String ASSET = "Asset";
+}
