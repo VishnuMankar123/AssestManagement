4d9323442a5c864bea58a069fe4a3cac723cbb01
Devang Hemant Bhagwat <devanghb@lentra.ai>
2023-06-23 23:00:26 +0530

    Added a new user-defined exception: FlywayTestException

---
+++ authorization_server/src/test/java/com/asset/authorization_server/exception/FlywayTestException.java
@@ -0,0 +1,7 @@
+package com.asset.authorization_server.exception;
+
+public class FlywayTestException extends RuntimeException {
+    public FlywayTestException(String message) {
+        super(message);
+    }
+}
