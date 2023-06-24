9e393c0d80b89fa363109f31fb20f0fb79f5394f
Devang Hemant Bhagwat <devanghb@lentra.ai>
2023-06-24 18:48:42 +0530

    Removed unused imports

--- authorization_server/src/test/java/com/asset/authorization_server/flyway/FlywayTest.java
+++ authorization_server/src/test/java/com/asset/authorization_server/flyway/FlywayTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.DatabaseMetaDataCallback;
 import org.springframework.jdbc.support.JdbcUtils;
 import org.springframework.jdbc.support.MetaDataAccessException;
