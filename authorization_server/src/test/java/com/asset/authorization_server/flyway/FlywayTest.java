4638fdf8717d39f1bc03255b5c919dd2a23bf02d
Devang Hemant Bhagwat <devanghb@lentra.ai>
2023-06-24 11:06:35 +0530

    Changed the test schema

--- authorization_server/src/test/java/com/asset/authorization_server/flyway/FlywayTest.java
+++ authorization_server/src/test/java/com/asset/authorization_server/flyway/FlywayTest.java
@@ -35,7 +35,7 @@ class FlywayTest {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    private static final String TEST_SCHEMA_NAME = "test_schema";
+    private static final String TEST_SCHEMA_NAME = "authorization_server_test_schema";
     private static final Set<String> tables = Set.of("clients",
             "authentication_methods",
             "grant_types",
