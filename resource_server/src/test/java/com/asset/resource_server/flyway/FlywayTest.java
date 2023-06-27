c4af3ba20143f1bceca37224e82475141c26164b
Devang Hemant Bhagwat <devanghb@lentra.ai>
2023-06-27 12:10:13 +0530

    Added a display name to the flyway test

--- resource_server/src/test/java/com/asset/resource_server/flyway/FlywayTest.java
+++ resource_server/src/test/java/com/asset/resource_server/flyway/FlywayTest.java
@@ -3,6 +3,7 @@ package com.asset.resource_server.flyway;
 import com.asset.resource_server.exception.FlywayTestException;
 import jakarta.annotation.Nonnull;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -46,6 +47,10 @@ class FlywayTest {
             "asset"
     );
 
+    @DisplayName(value = """
+            Unit test to check if
+            flyway created all the expected tables
+            """)
     @Test
     void databaseHasBeenInitialized() {
         GetTableNames getTableNames = new GetTableNames();
