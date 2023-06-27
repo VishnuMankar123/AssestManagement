bf80f24b2574c6b45a809ca4506c6a5d0455e83e
Devang Hemant Bhagwat <devanghb@lentra.ai>
2023-06-27 12:50:15 +0530

    Mended the delete API in the AssetController

--- resource_server/src/main/java/com/asset/resource_server/controller/AssetController.java
+++ resource_server/src/main/java/com/asset/resource_server/controller/AssetController.java
@@ -4,6 +4,7 @@ import com.asset.resource_server.data.AssetData;
 import com.asset.resource_server.service.AssetService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -42,8 +43,9 @@ public class AssetController {
         return this.assetService.findBySerialNumber(serialNumber);
     }
 
-    @DeleteMapping
-    public void remove(Long id) {
+    @DeleteMapping(path = "{id}")
+    @PreAuthorize(value = "hasAuthority('admin')")
+    public void remove(@PathVariable(name = "id") Long id) {
         this.assetService.deleteById(id);
     }
 }
